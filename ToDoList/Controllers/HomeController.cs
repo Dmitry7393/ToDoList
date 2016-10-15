733fccee5ed8afd68a375fa6fd368ed1d37c16b4
Dmitry7393 <dmitry7393@gmail.com>
2016-10-15 15:26:57 +0300

    Added table, updating and deleting notes

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(Note.mListNotes);
+        }
+       
+        public ActionResult AddNote(Note note)
+        {
+            if (note.Data != null)
+            {
+                note.PostDate = DateTime.Now.ToString();
+                Note.mListNotes.Add(note);
+            }
+            return PartialView("PartialViewNotes", Note.mListNotes);
+        }
+        
+        public ActionResult UpdateList(string note, string rowInTable)
+        {
+            int index = getIndexList(Convert.ToInt32(rowInTable));
+            Note.mListNotes[index].Data = note;
+            return PartialView("PartialViewNotes", Note.mListNotes);
+        }
+        
+        public ActionResult DeleteNote(string rowInTable)
+        {
+            int index = getIndexList(Convert.ToInt32(rowInTable));
+            Note.mListNotes.RemoveAt(index);
+            return PartialView("PartialViewNotes", Note.mListNotes);
+        }
+        private int getIndexList(int rowInTable)
+        {
+            return Note.mListNotes.Count - Convert.ToInt32(rowInTable);
+        }
+    }
+}
---
+++ ToDoList/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class Note
+    {
+        public static List<Note> mListNotes = new List<Note>();
+        //ID note
+        public int Id { get; set; }
+        //Note
+        public string Data { get; set; }
+        //post date
+        public string PostDate { get; set; }
+    }
+}
